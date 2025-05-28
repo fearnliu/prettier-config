6f3c433b0f9ddf82619ac5e346349bafaf81a971
Fearn Liu <16191685+fearnliu@users.noreply.github.com>
2025-05-28 10:58:09 +0800

    feat: specify config

---
+++ index.js
@@ -0,0 +1,11 @@
+const config = {
+  // Explicitly set configuration options whose default values differ across Prettier versions.
+  "arrowParens": "always",
+  "endOfLine": "lf",
+  "trailingComma": "all",
+  // Configuration options not explicitly specified will use Prettier's default values.
+  "singleAttributePerLine": true,
+  "vueIndentScriptAndStyle": true,
+};
+
+export default config;
